26a4fb6174dff4f22a17349e5e47a29e44b09512
kimnahyun005-tech <kimnahyun005@gmail.com>
2026-06-07 10:51:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ class ArtifactMapTransform:
 
 transform = transforms.Compose([
     transforms.Resize((380, 380)),
-    ArtifactMapTransform(blur_radius=2),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
