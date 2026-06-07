78e623207d0fac59f89c4e8d5d44c01d7504273e
kimnahyun005-tech <kimnahyun005@gmail.com>
2026-06-07 10:30:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ class ArtifactMapTransform:
         return Image.fromarray(artifact.astype(np.uint8))
 
 transform = transforms.Compose([
+    transforms.Resize((380, 380)),
     ArtifactMapTransform(blur_radius=2),
-    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
